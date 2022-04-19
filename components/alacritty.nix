55abf3a7e91ec89e3dd05574e84a1da94d90421a
Jeremy Stucki <dev@jeremystucki.ch>
2022-04-19 15:44:40 +0200

    Add alacritty workaround

---
+++ components/alacritty.nix
@@ -0,0 +1,43 @@
+{
+  home.file.".config/alacritty/alacritty.yml".source = ../resources/alacritty.yml;
+
+#   programs.alacritty = {
+#    enable = true;
+
+#     settings = {
+#       font = {
+#         size = 12.0;
+#         offset.y = 2;
+#       };
+
+#       colors = {
+#         primary = {
+#           background = "#2b2b2b";
+#           foreground = "#bbbbbb";
+#         };
+
+#         normal = {
+#           black   = "#ffffff";
+#           red     = "#ff6b68";
+#           green   = "#a8c023";
+#           yellow  = "#d6bf55";
+#           blue    = "#5394ec";
+#           magenta = "#ae8abe";
+#           cyan    = "#299999";
+#           white   = "#1f1f1f";
+#         };
+
+#         bright = {
+#           black   = "#575b70";
+#           red     = "#ff8785";
+#           green   = "#a8ca23";
+#           yellow  = "#ffff00";
+#           blue    = "#7eaef1";
+#           magenta = "#ff99ff";
+#           cyan    = "#6cdada";
+#           white   = "#e6e6e6";
+#         };
+#       };
+#     };
+#   };
+}
--- volt.nix
+++ volt.nix
@@ -1,5 +1,6 @@
 {
   imports = map (x: ./components + x) [
+    /alacritty.nix
     /zsh.nix
     /cli.nix
     /git.nix
@@ -13,6 +14,8 @@
   home.stateVersion = "22.05";
 
   programs.home-manager.enable = true;
+
+  home.file.".config/alacritty/machine-specific-config.yml".source = ./resources/alacritty-volt.yml;
 
   programs.git.includes = [
     {
@@ -28,44 +31,4 @@
   ];
   
   programs.zsh.initExtra = "bindkey '^[[3~' delete-char";
-
-  programs.alacritty = {
-    enable = true;
-
-    settings = {
-      font = {
-        size = 12.0;
-        offset.y = 2;
-      };
-
-      colors = {
-        primary = {
-          background = "#2b2b2b";
-          foreground = "#bbbbbb";
-        };
-
-        normal = {
-          black   = "#ffffff";
-          red     = "#ff6b68";
-          green   = "#a8c023";
-          yellow  = "#d6bf55";
-          blue    = "#5394ec";
-          magenta = "#ae8abe";
-          cyan    = "#299999";
-          white   = "#1f1f1f";
-        };
-
-        bright = {
-          black   = "#575b70";
-          red     = "#ff8785";
-          green   = "#a8ca23";
-          yellow  = "#ffff00";
-          blue    = "#7eaef1";
-          magenta = "#ff99ff";
-          cyan    = "#6cdada";
-          white   = "#e6e6e6";
-        };
-      };
-    };
-  };
 }
