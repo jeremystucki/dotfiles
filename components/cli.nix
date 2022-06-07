09ac507decaa5b448eaa16945abfab9f95eac185
Jeremy Stucki <dev@jeremystucki.ch>
2022-06-07 12:38:00 +0200

    Work on shell aliases

--- components/cli.nix
+++ components/cli.nix
@@ -16,8 +16,7 @@
   programs.jq.enable = true;
   programs.pandoc.enable = true;
 
-  programs.lsd = {
-    enable = true;
-    enableAliases = true;
-  };
+  programs.lsd.enable = true;
+  programs.zsh.shellAliases.ls = "lsd -l";
+  programs.zsh.shellAliases.lt = "lsd --tree";
 }
