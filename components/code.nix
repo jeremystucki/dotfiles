c6a96059bcbff51839896626edaf7ddaf504b60d
Jeremy Stucki <dev@jeremystucki.ch>
2022-10-25 09:05:50 +0200

    Update codium config

--- components/code.nix
+++ components/code.nix
@@ -20,5 +20,6 @@
   programs.vscode.userSettings = {
     vscode-neovim.neovimExecutablePaths.linux = "/home/jeremy/.nix-profile/bin/nvim";
     editor.fontFamily = "Input Sans";
+    editor.cursorSurroundingLines = 8;
   };
 }
