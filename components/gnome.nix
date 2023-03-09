3f894656c36b173542d572e19337bbb5d8ed0f1a
Jeremy Stucki <dev@jeremystucki.ch>
2023-03-09 09:32:45 +0100

    Update gnome keybindings

--- components/gnome.nix
+++ components/gnome.nix
@@ -8,14 +8,21 @@
   dconf.settings = {
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
       ];
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Super>Return";
-      command = "sh -c 'wmctrl -a alacritty || alacritty'";
+      command = "sh -c 'wmctrl -xa alacritty || alacritty'";
       name = "alacritty nix";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+      binding = "<Super>E";
+      command = "sh -c 'wmctrl -xa nautilus || nautilus /home/jeremy'";
+      name = "nautilus nix";
     };
   };
 }
