d5bc9f873811444fb4536bb332b24ccf7b91b6c4
Jeremy Stucki <dev@jeremystucki.ch>
2023-08-29 08:07:55 +0200

    Add some apps

--- components/apps.nix
+++ components/apps.nix
@@ -3,9 +3,13 @@
 {
   home.packages = with pkgs; [
     bookletimposer
+    blanket
+    citations
     evolution
+    gnome-decoder
     gnome.gnome-calendar
     gnome.gnome-contacts
+    gnome.gnome-characters
     inkscape
     obsidian
     spotify
