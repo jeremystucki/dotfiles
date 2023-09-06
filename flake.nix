4d33447e2c1ba24b6335896c9811be5e40d2149b
Jeremy Stucki <dev@jeremystucki.ch>
2023-09-06 17:27:01 +0200

    Add unstable pkgs

--- flake.nix
+++ flake.nix
@@ -3,13 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager }:
     let
       pkgs = import nixpkgs {
         system = "x86_64-linux";
@@ -18,14 +19,23 @@
           input-fonts.acceptLicense = true;
         };
       };
+      pkgs-unstable = import nixpkgs-unstable {
+        system = "x86_64-linux";
+      };
     in {
       homeConfigurations."jeremy@volt" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./volt.nix ];
+        extraSpecialArgs = {
+          inherit pkgs-unstable;
+        };       
       };
       homeConfigurations."jeremy@zephyr" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./zephyr.nix ];
+        extraSpecialArgs = {
+          inherit pkgs-unstable;
+        };       
       };
     };
 }
