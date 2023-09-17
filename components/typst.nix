4dcdd3641cf92432b9ac247ae8547922ee721e6d
Jeremy Stucki <dev@jeremystucki.ch>
2023-09-17 16:31:38 +0200

    Update typst config

--- components/typst.nix
+++ components/typst.nix
@@ -3,9 +3,11 @@
 {
   home.packages = with pkgs-unstable; [
     typst
+    typst-lsp
   ];
 
   programs.vscode.extensions = with pkgs-unstable.vscode-extensions; [
     nvarner.typst-lsp
+    tomoki1207.pdf
   ];
 }
