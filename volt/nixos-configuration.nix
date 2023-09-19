9c71dc2f8dac20dfa611847f345932269288ad51
Jeremy Stucki <dev@jeremystucki.ch>
2023-09-19 21:19:21 +0200

    Trust tailscale

--- volt/nixos-configuration.nix
+++ volt/nixos-configuration.nix
@@ -95,6 +95,7 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.firewall.trustedInterfaces = [ "tailscale0" ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
