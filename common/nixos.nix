af62f822cc3d7698af91d25c417e21ae4b222b82
Jeremy Stucki <dev@jeremystucki.ch>
2023-11-28 22:10:56 +0100

    Extract common nixos configuration

---
+++ common/nixos.nix
@@ -0,0 +1,110 @@
+{ config, pkgs, pkgs-unstable, ... }:
+
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+
+    initrd.secrets = {
+      "/crypto_keyfile.bin" = null;
+    };
+
+    supportedFilesystems = [ "ntfs" ];
+  };
+
+  users.users.jeremy = {
+    isNormalUser = true;
+    description = "Jeremy Stucki";
+    extraGroups = [ "networkmanager" "wheel" "i2c" "docker" ];
+  };
+
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  networking = {
+    networkmanager.enable = true;
+    firewall.trustedInterfaces = [ "tailscale0" ];
+    # wireless.enable = true;
+  };
+
+  # time.timeZone = "Europe/Zurich";
+  i18n.defaultLocale = "en_US.UTF-8";
+  
+  services.xserver = {
+    enable = true;
+
+    layout = "us";
+    xkbVariant = "";
+
+    displayManager = { gdm.enable = true; };
+    desktopManager = { gnome.enable = true; };
+  };
+
+  services.flatpak.enable = true;
+  services.openssh.enable = true;
+  services.printing.enable = true;
+  services.tailscale.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  virtualisation.docker.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  hardware.i2c.enable = true;
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  programs.steam.enable = true;
+
+  programs.java = {
+    enable = true;
+    package = pkgs.openjdk17;
+  };
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "jeremy" ];
+  };
+
+  programs.ssh.extraConfig = ''
+    Host *
+        IdentityAgent ${config.users.users.jeremy.home}/.1password/agent.sock
+  '';
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    TZDIR = "/etc/zoneinfo";
+  };
+}
--- volt/nixos-configuration.nix
+++ volt/nixos-configuration.nix
@@ -2,72 +2,18 @@
 
 {
   imports = [
+    ../common/nixos.nix
     ./nixos-hardware-configuration.nix
   ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Setup keyfile
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
 
   # Enable swap on luks
   boot.initrd.luks.devices."luks-e0d59253-c3ee-4fab-8ce0-182877db57c3".device = "/dev/disk/by-uuid/e0d59253-c3ee-4fab-8ce0-182877db57c3";
   boot.initrd.luks.devices."luks-e0d59253-c3ee-4fab-8ce0-182877db57c3".keyFile = "/crypto_keyfile.bin";
 
-  boot.supportedFilesystems = [ "ntfs" ];
-
   networking.hostName = "volt-nixos";
-  networking.networkmanager.enable = true;
-  # networking.wireless.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Zurich";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jeremy = {
-    isNormalUser = true;
-    description = "Jeremy Stucki";
-    extraGroups = [ "networkmanager" "wheel" "i2c" "docker" ];
     packages = with pkgs; [
       alacritty
       chromium
@@ -76,6 +22,7 @@
       jetbrains.rider
       nodejs
       nodePackages.pnpm
+      nvd
       slack
       telegram-desktop
       wl-clipboard
@@ -84,31 +31,6 @@
     ];
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  programs.java = {
-    enable = true;
-    package = pkgs.openjdk17;
-  };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-  networking.firewall.trustedInterfaces = [ "tailscale0" ];
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -116,13 +38,6 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05";
-
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
@@ -153,41 +68,10 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
-  hardware.i2c.enable = true;
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  services.flatpak.enable = true;
-
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-
-  services.tailscale.enable = true;
-
-  programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-    # Certain features, including CLI integration and system authentication support,
-    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
-    polkitPolicyOwners = [ "jeremy" ];
-  };
-
-  programs.ssh.extraConfig = ''
-    Host *
-        IdentityAgent ${config.users.users.jeremy.home}/.1password/agent.sock
-  '';
-
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  environment.sessionVariables.TZDIR = "/etc/zoneinfo";
-
-  virtualisation.docker.enable = true;
-
   environment.systemPackages = with pkgs.gnomeExtensions; [
     adjust-display-brightness
     blur-my-shell
     dash-to-dock
     just-perfection
   ];
-
-  programs.steam.enable = true;
 }
