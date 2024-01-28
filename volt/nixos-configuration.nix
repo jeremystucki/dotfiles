c0e0000ebe8286f5e81e13990949135eae10888a
Jeremy Stucki <dev@jeremystucki.ch>
2024-01-28 14:53:29 +0100

    Move state version to common config

--- volt/nixos-configuration.nix
+++ volt/nixos-configuration.nix
@@ -15,14 +15,6 @@
 
   networking.hostName = "volt-nixos";
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05";
-
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
