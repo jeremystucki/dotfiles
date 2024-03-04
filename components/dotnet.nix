8d3a159cc0da284b3535d5b3613ae50e91c495df
Jeremy Stucki <dev@jeremystucki.ch>
2024-03-04 21:53:45 +0100

    Fix dotnet config

--- components/dotnet.nix
+++ components/dotnet.nix
@@ -1,15 +1,18 @@
 { pkgs, pkgs-unstable, ... }:
 
 {
-  home.packages = with pkgs.dotnetCorePackages; [
-    combinePackages
-    [
-      dotnet-sdk_6
-      dotnet-aspnetcore_6
-      dotnet-sdk_7
-      dotnet-aspnetcore_7
-      pkgs-unstable.dotnet-sdk_8
-      pkgs-unstable.dotnet-aspnetcore_8
-    ]
+  home.packages = [
+    (
+      with pkgs;
+      with dotnetCorePackages;
+      combinePackages [
+        dotnet-sdk_6
+        dotnet-aspnetcore_6
+        dotnet-sdk_7
+        dotnet-aspnetcore_7
+        pkgs-unstable.dotnet-sdk_8
+        pkgs-unstable.dotnet-aspnetcore_8
+      ]
+    )
   ];
 }
