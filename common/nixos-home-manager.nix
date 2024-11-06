20888204fc0a149631af12111cb58f782134acbe
Jeremy Stucki <dev@jeremystucki.ch>
2024-11-06 11:28:58 +0100

    Make commit signing work on darwin

--- common/nixos-home-manager.nix
+++ common/nixos-home-manager.nix
@@ -1,8 +1,5 @@
 { pkgs, ... }:
 
-let
-  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG9keparNqpev2qrDO3cAiDzyTUsAAN9Mh+JLbOsdiZs";
-in
 {
   imports = map (x: ../components + x) [
     /common-apps.nix
@@ -11,19 +8,4 @@ in
   ];
 
   home.homeDirectory = "/home/jeremy";
-
-  programs.git.extraConfig = {
-    commit.gpgsign = true;
-    user.signingkey = publicKey;
-    "gpg \"ssh\"".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
-
-    gpg = {
-      format = "ssh";
-      ssh.allowedSignersFile = builtins.toFile "allowed_signers" ''
-        dev@jeremystucki.ch ${publicKey}
-        jeremy.stucki@valora.com ${publicKey}
-        jeremy.stucki@ost.ch ${publicKey}
-      '';
-    };
-  };
 }
--- components/git.nix
+++ components/git.nix
@@ -2,6 +2,7 @@
 
 let
   gitPackage = pkgs.git;
+  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG9keparNqpev2qrDO3cAiDzyTUsAAN9Mh+JLbOsdiZs";
 in
 {
   home.shellAliases = {
@@ -57,6 +58,24 @@ in
         autostash = true;
         autosquash = true;
         updateRefs = true;
+      };
+
+      commit.gpgsign = true;
+      user.signingkey = publicKey;
+
+      "gpg \"ssh\"".program =
+        if pkgs.stdenv.isDarwin then
+          "/Applications/1Password.app/Contents/MacOS/op-ssh-sig"
+        else
+          "${pkgs._1password-gui}/bin/op-ssh-sign";
+
+      gpg = {
+        format = "ssh";
+        ssh.allowedSignersFile = builtins.toFile "allowed_signers" ''
+          dev@jeremystucki.ch ${publicKey}
+          jeremy.stucki@valora.com ${publicKey}
+          jeremy.stucki@ost.ch ${publicKey}
+        '';
       };
     };
 
