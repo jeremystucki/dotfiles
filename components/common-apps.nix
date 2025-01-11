4c5f24d6853a0c7b7068ad8964d02e2b231b5c34
Jeremy Stucki <dev@jeremystucki.ch>
2025-01-11 20:15:53 +0100

    Add pdfarranger

--- components/common-apps.nix
+++ components/common-apps.nix
@@ -1,25 +1,30 @@
 { pkgs, pkgs-unstable, ... }:
 
 {
-  home.packages = with pkgs; [
-    bambu-studio
-    blanket
-    evolution
-    gnome-decoder
-    gnome-calendar
-    gnome-characters
-    gnome-contacts
-    handbrake
-    imagemagick
-    inkscape
-    libreoffice
-    mpv
-    obsidian
-    piper
-    cartridges
-    pkgs-unstable.slack
-    threema-desktop
-    wireshark-qt
-    zathura
-  ];
+  home.packages =
+    with pkgs;
+    [
+      bambu-studio
+      blanket
+      evolution
+      gnome-decoder
+      gnome-calendar
+      gnome-characters
+      gnome-contacts
+      handbrake
+      imagemagick
+      inkscape
+      libreoffice
+      mpv
+      obsidian
+      piper
+      cartridges
+      pkgs-unstable.slack
+      threema-desktop
+      wireshark-qt
+      zathura
+    ]
+    ++ lib.optionals (!stdenv.isDarwin) [
+      pdfarranger
+    ];
 }
