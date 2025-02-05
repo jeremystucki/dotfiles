723fa210d079560b977fad312f270566585fea2f
Jeremy Stucki <dev@jeremystucki.ch>
2025-02-05 16:54:35 +0100

    Add new employer

--- components/git.nix
+++ components/git.nix
@@ -3,6 +3,7 @@
 let
   gitPackage = pkgs.git;
   publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG9keparNqpev2qrDO3cAiDzyTUsAAN9Mh+JLbOsdiZs";
+  rsaPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDIvAX/wQdFT4h2ifgPfi2JxZzKcDYY60uM9PRtbyoMXNc73QXvIVHs3wFB3sg3nISX745Et9z/FZUUYZZ0qey7jyRuzXIa7KH3xdDRJEj+JyZqr4cSvHcVSf5cYfkFRRGmaMPqdr4Q+52S/KtGNIBbH+GZlXk5T7GnmMn9f+EPBeggJ1CKAJoWctfbLcTQ9M7+ZIuAJKPMpPWuBL2Z7KVUmQPVzHvOfIybMBIDAuTotVpcJDgnKpw4I/Qang49RlT8Y3HxToIiPOZasAD/+bXktYzf3zFl+dpljfecX9tdWmF+DwXVMjhFNoY9w9RYKIU3KT3TEdrx45tDaNKK2wZ4o6elQyYwyQC8omAdqk91QxcZvaq85FKdRgdk04gZogQRSceDhRE6GAhQQGIn1Ne9MPxcElHpsniiJLJRtm9GXc1xZ25A4VAbZK4KRuC2AXeJhfYRdDp7QRobsY+cRXW0JcMVVnBkI34Az5kV05NUxifSNOFCPReBkWS5F/nHvu65qQaGg4nKCDVotHHcTDB8zjg6DPEOx2rAf8eN0PNkAcguy+y+/HylBeaj0fyIOKMoAutO5NiGiXLfzT/mJ35wv3rwx4ynBKocWB9qXZWVkutOkYtp60nTgLLz6isy1K0qJ1LL1YvTLXy/kiFn1N0Q3uTHC1VJnDkcZgXvMTxdGQ==";
 in
 {
   home.shellAliases = {
@@ -74,6 +75,7 @@ in
         ssh.allowedSignersFile = builtins.toFile "allowed_signers" ''
           dev@jeremystucki.ch ${publicKey}
           jeremy.stucki@ost.ch ${publicKey}
+          jeremy.stucki@digitecgalaxus.ch ${rsaPublicKey}
         '';
       };
     };
@@ -98,6 +100,13 @@ in
         condition = "hasconfig:remote.*.url:ssh://git@gitlab.ost.ch:*/**";
         contents = {
           user.email = "jeremy.stucki@ost.ch";
+        };
+      }
+      {
+        condition = "hasconfig:remote.*.url:ssh://git@ssh.dev.azure.com:*/**";
+        contents = {
+          user.email = "jeremy.stucki@digitecgalaxus.ch";
+          user.signingkey = rsaPublicKey;
         };
       }
     ];
