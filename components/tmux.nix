cb766752d75d561a5e24911197b4b127f633d36e
Jeremy Stucki <dev@jeremystucki.ch>
2025-05-24 14:47:07 +0200

    Update catppuccin tmux plugin

--- components/tmux.nix
+++ components/tmux.nix
@@ -17,8 +17,10 @@
       {
         plugin = catppuccin;
         extraConfig = ''
-          set -g @catppuccin_left_separator "█"
-          set -g @catppuccin_right_separator "█"
+          set -g @catppuccin_flavor "mocha"
+          set -g @catppuccin_window_status_style "rounded"
+          set -g @catppuccin_window_current_text " #T"
+          set -g @catppuccin_window_text " #T"
         '';
       }
       {
@@ -48,13 +50,28 @@
       set-window-option -g pane-base-index 1
       set-option -g renumber-windows on
 
-      set-option -g status-style bg=white,fg=cyan
-
       bind c new-window -c "#{pane_current_path}"
       bind '"' split-window -v -c "#{pane_current_path}"
-      bind % split-window -h -c "#{pane_current_path}"
+      bind '%' split-window -h -c "#{pane_current_path}"
 
       bind s choose-tree -s -O name
+
+      set -g status-left ""
+      set -g status-right-length 100
+      set -g status-right "#{E:@catppuccin_status_session}"
     '';
   };
+
+  programs.zsh.initContent = ''
+    if [[ -n $TMUX ]]; then
+      autoload -Uz add-zsh-hook
+
+      tmux_report_cwd() {
+        tmux select-pane -t "$TMUX_PANE" -T "$(basename $PWD)"
+      }
+
+      add-zsh-hook chpwd tmux_report_cwd
+      tmux_report_cwd
+    fi
+  '';
 }
